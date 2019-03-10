ad2015c7fc40eeb76493fb33527d4905d7b1b6c7
Pranay Sharma <pranay@pranaysharma.com>
2019-03-10 19:32:24 -0400

    3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from '../product.service'
+import {Album} from '../album'
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
@@ -7,11 +9,10 @@ import {ProductService} from '../product.service'
 })
 export class ProductDescriptionComponent implements OnInit {
   
-  albumInfo;
+  albumInfo:Album;
   constructor(private _productService:ProductService) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
-
 }
